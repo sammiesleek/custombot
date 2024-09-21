cf01f603a370ded1bddbc99f8e1179dd8ab6e61d
Sammy Ajayi <sammyajayi@gmail.com>
2024-09-21 16:47:45 +0100

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const LocalSession = require("telegraf-session-local");
 const express = require("express");
 const bodyParser = require("body-parser");
-const token = process.env.BOT_TOKEN
+const token = "7885430459:AAGpsZbmfL7ZlqzQM9JKTwAJmmgkd8YYXgo"
 const bot = new Telegraf(token); // Use environment variable for bot token
 
 // Middleware for user sessions
@@ -25,11 +25,11 @@ app.post(`/webhook/${token}`, (req, res) => {
 
 
 // setting web hoook 
-// bot.telegram.setWebhook(`https://aa7a-105-119-3-94.ngrok-free.app/webhook/${token}`,{allowed_updates: JSON.stringify(["message", "edited_channel_post", "callback_query", "message_reaction", "message_reaction_count","message", "chat_member"])}).then((info => console.log(info)));
+ //bot.telegram.setWebhook(`https://c1e9-105-119-3-94.ngrok-free.app/webhook/${token}`,{allowed_updates: JSON.stringify(["message", "edited_channel_post", "callback_query", "message_reaction", "message_reaction_count","message", "chat_member"])}).then((info => console.log(info)));
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, (res) => {
   console.log(`Server running on port ${PORT}`);
 });
@@ -44,7 +44,7 @@ function generateMathProblem() {
 
 // Handle new chat members with a captcha challenge
 bot.on("chat_member", async (ctx) => {
-  console.log(ctx.chatMember.chat.id)
+  console.log(ctx.chatMember.chat.username)
 
    const newMember = ctx.chatMember.new_chat_member
     if (!newMember.user.is_bot && newMember.status == "member") {
@@ -60,18 +60,12 @@ bot.on("chat_member", async (ctx) => {
         thisChatId:ctx.chatMember.chat.id
       };
 
-      // Mute the new member
-      //   await ctx.telegram.restrictChatMember(ctx.chat.id, newMember.id, {
-      //     can_send_messages: false,
-      //   });
-
+      
       // Send the captcha challenge
       const challengeMessage = await ctx.replyWithHTML(
-        `Welcome to our community. Please prove to me that you are a human by solving this simple math task: <b>${mathProblem.question}</b>`
+        `Hello @${newMember.user.username} , Welcome to our community. Please prove to me that you are a human by solving this simple math task: <b>${mathProblem.question}</b>`
       );
-      //   const challengeMessage = await ctx.replyWithHTML(
-      //     `Welcome <b>${newMember.first_name}</b>! 🚨\n\nBefore you can chat, solve this math problem: <b>${mathProblem.question}</b>\n\nYou have 3 attempts!`
-      //   );
+     
 
       ctx.session.captcha.challengeMessageId = challengeMessage.message_id;
       ctx.session.captcha.challengechatId = challengeMessage.chat.id;
@@ -94,11 +88,25 @@ bot.on("chat_member", async (ctx) => {
  
 });
 
-// Handle responses to the captcha challenge
-bot.on("message", async (ctx) => {
-  console.log(ctx)
-const userSession = ctx.session.captcha;
 
+const getAdmins = async (ctx) => {
+  try {
+    const res = await ctx.getChatAdministrators(ctx.message.chat.id);
+
+    if (res) {
+      return res; 
+    } else {
+      return []; 
+    }
+  } catch (error) {
+    console.error("Error fetching admins:", error);
+    return []; 
+  }
+};
+
+// handle users response to captcha 
+const handleCaptcahResponse = async (ctx) => {
+  const userSession = ctx.session.captcha
   if (userSession && userSession.status === "pending") {
     const userAnswer = parseInt(ctx.message.text, 10);
     ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id);
@@ -119,15 +127,109 @@ const userSession = ctx.session.captcha;
       }
     }
   }
+}
+
+const preventspam = async (ctx) => {
+
+  const admins =await getAdmins(ctx)
+  let adminTags = []
+  const senderTag = ctx.message.from.username
+  admins.forEach(admin => {
+    adminTags.push(admin.user.username)
+  });
+  if (!adminTags.includes(senderTag) && ctx.message.text.match(/(^|\s)@\w+/)) {
+    const tagText = ctx.message.text.match(/(^|\s)@\w+/)[0].replace(/@/g,'').trim()
+    
+      
+    if (!adminTags.includes(tagText)) {
+      console.log("first")
+      
+        await ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id)
+      
+    }
+  }
+}
+
+// Handle new messages
+bot.on("message", async (ctx) => {
+
+handleCaptcahResponse(ctx)
+
+  if (ctx.message.message_id && ctx.message.text) {
+    
+    preventspam(ctx)
+  }
+
+
 });
 
-// Uncomment if not using webhooks
-// if (process.env.USE_WEBHOOK !== "true") {
-// bot.launch();
-// }
 
 // Utility endpoint to manually check webhook info
 app.get("/webhook-info", async (req, res) => {
   const info = await bot.telegram.getWebhookInfo();
   res.json(info);
 });
+
+
+
+
+[
+  {
+    user: {
+      id: 7885430459,
+      is_bot: true,
+      first_name: 'Tester',
+      username: 'Tester_my_bot_bot'
+    },
+    status: 'administrator',
+    can_be_edited: false,
+    can_manage_chat: true,
+    can_change_info: true,
+    can_delete_messages: true,
+    can_invite_users: true,
+    can_restrict_members: true,
+    can_pin_messages: true,
+    can_manage_topics: false,
+    can_promote_members: false,
+    can_manage_video_chats: true,
+    can_post_stories: true,
+    can_edit_stories: true,
+    can_delete_stories: true,
+    is_anonymous: false,
+    can_manage_voice_chats: true
+  },
+  {
+    user: {
+      id: 5391044038,
+      is_bot: false,
+      first_name: 'Raphaël',
+      username: 'Raphal_let'
+    },
+    status: 'administrator',
+    can_be_edited: false,
+    can_manage_chat: true,
+    can_change_info: true,
+    can_delete_messages: true,
+    can_invite_users: true,
+    can_restrict_members: true,
+    can_pin_messages: true,
+    can_manage_topics: false,
+    can_promote_members: true,
+    can_manage_video_chats: true,
+    can_post_stories: true,
+    can_edit_stories: true,
+    can_delete_stories: true,
+    is_anonymous: false,
+    can_manage_voice_chats: true
+  },
+  {
+    user: {
+      id: 1131179178,
+      is_bot: false,
+      first_name: 'Sammy Ajayi || Artyfact',
+      username: 'sammieajayi'
+    },
+    status: 'creator',
+    is_anonymous: false
+  }
+]
